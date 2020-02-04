261e0cfef0df819e09e7ae22f65cb8cc23d9d306
nekinci <niyaziekinci5050@gmail.com>
2020-02-04 17:39:48 +0300

    sdg

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -20,7 +20,7 @@ namespace WebAPI
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseUrls("http://*:80");
                 });
     }
 }
